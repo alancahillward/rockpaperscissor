de92387f2cc926f11d0052571ad359300f7cf350
Alan Ward <87635057+2knives@users.noreply.github.com>
2022-10-30 23:38:36 -0500

    Add files via upload

---
+++ rockpaperscissors.py
@@ -0,0 +1,40 @@
+import random
+
+
+while True: 
+    choices = ["rock", "paper", "scissors"]     
+    compchoice = random.choice(choices)
+    player = input("rock, paper or scissors?: ").lower()
+
+    lost = "You Lose :( "
+    won = "You Won!"
+
+    if player == compchoice:
+        print('Tie!')
+    elif player == "rock":
+        if compchoice == "scissors":
+            print(won)
+        else:
+            print(lost)
+    elif player == "paper":
+        if compchoice == "rock":
+            print(won)
+        else:
+            print (lost)
+    elif player == "scissors":
+        if compchoice == "paper":
+            print(won)
+        else:
+            print(lost)
+
+
+    print("computer: " + compchoice)
+    print("player: "+ player)
+
+    play_again = input("Play again? (yes/no): ").lower()
+
+    if play_again != "yes":
+        break
+print("Bye!")
+
+
